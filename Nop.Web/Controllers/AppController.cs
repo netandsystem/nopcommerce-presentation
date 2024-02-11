8d3867568ceff8d4a0b30bf95f261e3df7da9f8f
gustavo-dgs <gustavo.dgs19@gmail.com>
2024-02-10 22:09:41 -0400

    agregando link para app

---
+++ Nop.Web/Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nop.Web.Controllers;
+
+[AutoValidateAntiforgeryToken]
+public class AppController : Controller
+{
+    public IActionResult Index()
+    {
+        return File()
+    }
+}
